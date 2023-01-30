27eb92ccfe3a8dd15263cfa210a13a23ef1fb349
Qaisar Abbas <122449496+qaisar786abbas@users.noreply.github.com>
2023-01-30 06:49:53 -0800

    add index.html and app.js file

---
+++ JS-2/app.js
@@ -0,0 +1,60 @@
+//Chapter-2
+
+//String & Variable
+
+//Question-no 1
+// var userName;
+
+//Question-no 2
+// var myName = " Qaisar Abbas ";
+
+//question-no 3 (a)
+// var message;
+
+//question-no 3 (b)
+// var message = "Hello World"; 
+
+//Question-no 3 (c)
+// alert(message);
+
+//Question-no 4
+// var name = "Qaisar Abbas";
+// alert(name);
+// var age = "I am 19 year old";
+// alert(age);
+// var profession = "he is a web developer";
+// alert(profession);
+
+//Question-no 5
+// var food = "pizza \npizz \npiz \npi \np";
+// alert(food);
+
+// Question-no 6
+// var email = "My email is";
+// email = "My email is" + "example@example.com";
+// alert(email);
+
+// Question-no 7
+// var book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from book A smarter way to learn JavaScript");
+
+// Question-no 8
+// document.write("YAH! I can write HTML content through JavaScript");
+
+//Question-no 9
+// var image ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
